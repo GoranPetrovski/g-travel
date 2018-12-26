6528a7b90cf36f2766c46a7ba33b8b3389acfb3d
goran.petrovski <goki.petrovski@gmail.com>
2018-12-26 17:07:03 +0100

    - create profile integration test for getAll profiles endpoint

--- src/main/java/com/spring5/webflux/demo/web/ProfileRouter.java
+++ src/main/java/com/spring5/webflux/demo/web/ProfileRouter.java
@@ -1,21 +1,32 @@
 package com.spring5.webflux.demo.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
-public class ProfileEndpointConfiguration {
+public class ProfileRouter {
+
+    @Autowired
+    private ProfileHandler handler;
 
     @Bean
-    RouterFunction<ServerResponse> routes(ProfileHandler handler) {
+    RouterFunction<ServerResponse> routes() {
         return route(GET("/profiles"), handler::all)
                 .andRoute(GET("/profiles/{id}"), handler::getById)
                 .andRoute(DELETE("/profiles/{id}"), handler::deleteById)
                 .andRoute(PUT("/profiles/{id}"), handler::updateById);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> getProfiles() {
+        return RouterFunctions.route(GET("/profiles").and(accept(MediaType.APPLICATION_JSON_UTF8)), handler::all);
     }
 }
---
+++ src/test/java/com/spring5/webflux/demo/web/ProfileEndpointConfigurationTest.java
@@ -0,0 +1,68 @@
+package com.spring5.webflux.demo.web;
+
+import com.spring5.webflux.demo.BookserviceApplication;
+import com.spring5.webflux.demo.models.Profile;
+import com.spring5.webflux.demo.repositories.ProfileRepository;
+import lombok.extern.log4j.Log4j2;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+
+import static org.mockito.BDDMockito.given;
+
+@Log4j2
+//@WebFluxTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = BookserviceApplication.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ProfileEndpointConfigurationTest {
+
+    @Autowired
+    private ProfileRouter profileRouter;
+
+    @MockBean
+    private ProfileRepository profileRepository;
+
+    @Test
+    public void getAll() {
+        log.info("running " + this.getClass().getName());
+        WebTestClient client = WebTestClient
+                .bindToRouterFunction(profileRouter.getProfiles())
+                .build();
+
+
+        given(profileRepository.findAll())
+                .willReturn(Flux.just(
+                        new Profile("1", "A"),
+                        new Profile("2", "B")
+                ));
+
+        Mockito.when(profileRepository.findAll())
+                .thenReturn(Flux.just(
+                        new Profile("1", "A"),
+                        new Profile("2", "B")
+                ));
+
+        client.get()
+                .uri("/profiles")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBody()
+                .jsonPath("$.[0].id").isEqualTo("1")
+                .jsonPath("$.[0].email").isEqualTo("A")
+                .jsonPath("$.[1].id").isEqualTo("2")
+                .jsonPath("$.[1].email").isEqualTo("B");
+
+    }
+}
