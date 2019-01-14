8e11937899b17b46f8a8976eb0745b5aac770d5e
goran.petrovski <goki.petrovski@gmail.com>
2019-01-14 14:31:07 +0100

    - create separate Post header and router using webflux

--- src/main/java/com/spring5/webflux/demo/bootstrap/Bootstrap.java
+++ src/main/java/com/spring5/webflux/demo/bootstrap/Bootstrap.java
@@ -1,37 +1,106 @@
 package com.spring5.webflux.demo.bootstrap;
 
+import com.spring5.webflux.demo.helpers.PostId;
 import com.spring5.webflux.demo.models.Book;
+import com.spring5.webflux.demo.models.Comment;
+import com.spring5.webflux.demo.models.Post;
+import com.spring5.webflux.demo.models.User;
 import com.spring5.webflux.demo.repositories.BookRepository;
+import com.spring5.webflux.demo.repositories.CommentRepository;
+import com.spring5.webflux.demo.repositories.PostRepository;
+import com.spring5.webflux.demo.repositories.UserRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 
+import java.util.Arrays;
+import java.util.List;
+
+@Log4j2
 @Component
 public class Bootstrap implements CommandLineRunner {
 
     @Autowired
     private BookRepository bookRepository;
 
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public void run(String... args) throws Exception {
         System.out.println("RUNNNN");
         if (bookRepository.count().block() == 0) {
-            bookRepository.save(Book.builder()
-                    .author("Dan Brown")
-                    .title("Deception Point").build()).block();
-
-            System.out.println("Loaded books " + bookRepository.count().block());
-
-            bookRepository.save(Book.builder()
-                    .author("Dan Brown")
-                    .title("Disgital Forests=").build()).block();
-
-            bookRepository.save(Book.builder()
-                    .author("Dan Brown")
-                    .title("Angels and Demons").build()).block();
-
-            System.out.println("Loaded books " + bookRepository.count().block());
+            this.booksInitialization();
         }
+        if(postRepository.count().block() == 0){
+            this.postsInitialization();
+        }
+        this.userInitialization();
+    }
 
+    public void userInitialization() {
+        userRepository
+                .deleteAll()
+                .thenMany(
+                        Flux
+                                .just("user", "admin")
+                                .flatMap(
+                                        username -> {
+                                            List<String> roles = "user".equals(username)
+                                                    ? Arrays.asList("ROLE_USER")
+                                                    : Arrays.asList("ROLE_USER", "ROLE_ADMIN");
+
+                                            User user = User.builder()
+                                                    .roles(roles)
+                                                    .username(username)
+                                                    .password(passwordEncoder.encode("password"))
+                                                    .email(username + "@example.com")
+                                                    .build();
+                                            return userRepository.save(user);
+                                        }
+                                )
+                )
+                .log()
+                .subscribe(
+                        null,
+                        null,
+                        () -> log.info("done users initialization...")
+                );
+    }
+
+    public void postsInitialization() {
+        Post post = postRepository.save(Post.builder()
+                .title("The most beautiful country")
+                .content("Country with ...").build()).block();
+
+        commentRepository.save(Comment.builder()
+                .content("Yeah! It's very realistic post")
+                .post(new PostId(post.getId())).build()).block();
+    }
+
+    public void booksInitialization() {
+        bookRepository.save(Book.builder()
+                .author("Dan Brown")
+                .title("Deception Point").build()).block();
+
+        bookRepository.save(Book.builder()
+                .author("Dan Brown")
+                .title("Disgital Forests").build()).block();
+
+        bookRepository.save(Book.builder()
+                .author("Dan Brown")
+                .title("Angels and Demons").build()).block();
     }
 }
