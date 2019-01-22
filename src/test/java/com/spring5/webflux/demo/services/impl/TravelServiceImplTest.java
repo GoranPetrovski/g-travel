7b7c8fa18f613d9c9134c0005d72c830651839ef
goran.petrovski <goki.petrovski@gmail.com>
2019-01-22 16:26:10 +0100

    - using stepVerifier for testing travel service layer

--- src/test/java/com/spring5/webflux/demo/services/impl/TravelServiceImplTest.java
+++ src/test/java/com/spring5/webflux/demo/services/impl/TravelServiceImplTest.java
@@ -7,8 +7,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.util.StringUtils;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import java.time.LocalDate;
 
@@ -31,11 +33,15 @@ public class TravelServiceImplTest {
 
     private final Mono<Travel> travelMono = Mono.just(TRAVEL);
 
+    private final Travel.Type SELECTED_TYPE = Travel.Type.TRAVELER;
+
     @BeforeEach
     void setMockOutput() {
         MockitoAnnotations.initMocks(this);
         when(travelService.all()).thenReturn(MOCK_TRAVELS);
         when(travelService.create(TRAVEL)).thenReturn(travelMono);
+        when(travelService.getById("1")).thenReturn(travelMono);
+        when(travelService.getByType(SELECTED_TYPE)).thenReturn(MOCK_TRAVELS.filter(t -> t.getType().equals(Travel.Type.TRAVELER)));
     }
 
     @Test
@@ -46,5 +52,27 @@ public class TravelServiceImplTest {
     @Test
     public void create() {
         assertEquals(travelService.create(TRAVEL), travelMono);
+
+        StepVerifier.create(travelService.create(TRAVEL))
+                .expectNextMatches(saved -> StringUtils.hasText(saved.getId()))
+                .verifyComplete();
+    }
+
+    @Test
+    public void getOne() {
+        StepVerifier.create(travelService.getById("1"))
+                .expectNextMatches(travel -> {
+                    assertEquals(travel.getDate(), LocalDate.now().plusDays(1));
+                    return true;
+                }).expectComplete().verify();
+    }
+
+    @Test
+    public void getByType() {
+        StepVerifier.create(travelService.getByType(SELECTED_TYPE))
+                .expectNextMatches(travel -> {
+                    assertEquals(travel.getType(), SELECTED_TYPE);
+                    return true;
+                }).expectComplete().verify();
     }
 }
